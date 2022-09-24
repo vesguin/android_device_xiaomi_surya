fdfc900d5530590be0c38369e1849cf57cb0e25a
Richard Raya <rdxzv.dev@gmail.com>
2022-09-23 22:40:07 -0300

    surya: Properly name vintf folder

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,12 +44,12 @@ TARGET_NO_BOOTLOADER := true
 TARGET_TAP_TO_WAKE_NODE  := "/proc/tp_gesture"
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/configs/hidl/device_framework_compatibility_matrix.xml
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/device_framework_compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE += device/qcom/common/compatibility_matrix.xml
 
 ODM_MANIFEST_SKUS += surya
-ODM_MANIFEST_SURYA_FILES := $(DEVICE_PATH)/configs/hidl/manifest-nfc.xml
+ODM_MANIFEST_SURYA_FILES := $(DEVICE_PATH)/configs/vintf/manifest-nfc.xml
 
 # HWUI
 HWUI_COMPILE_FOR_PERF := true
